f317e37bb37e65cccbc420f65f09d0078119442f
Josh Vinlove <jjvinlove@gmail.com>
2019-10-02 10:50:43 -0600

    displayed metrics

--- js/app.js
+++ js/app.js
@@ -1,13 +1,25 @@
 class Gotchi {
 	constructor () {
-	this.hunger= 0,
-	this.sleepiness= 0,
-	this.boredom= 0,
+	this.hunger= 1,
+	this.sleepiness= 1,
+	this.boredom= 1,
 	this.age= 0
 	}
+
+
 };
+
+const game = {
+	hunger: 1,
+	sleepiness: 1,
+	boredom: 1,
+	age: 0
+
+	setUpRound(){
+}
 
 const joshagotchi = new Gotchi();
 
+const $hunger = $('.hunger');
 
 
