119ecce9c687de35e5dfd750b8b7869d6ca08b79
Josh Vinlove <jjvinlove@gmail.com>
2019-10-03 10:50:48 -0600

    animation complete

--- js/app.js
+++ js/app.js
@@ -37,16 +37,23 @@ const game = {
 	sleepiness: 1,
 	boredom: 1,
 	age: 0,
+	gameOver: false,
 
 
 
 	setAge () {
 
-	const interval = setInterval(() => { 
-	const $age = $('#age');
-          this.age++
+		const interval = setInterval(() => { 
+		const $age = $('#age');
+
+          this.age++;
        
         $('#age').text(`Age: ${this.age}`);
+        
+        if (this.gameOver === true){
+        	clearInterval(interval);
+        	$('img').css("animation-iteration-count", "0");
+        }
         if (this.age >=2 && this.age <4) {
         	$('img').attr("src", "https://i.pinimg.com/originals/bd/ce/47/bdce478c4108aee6d2ac5843d966938d.jpg");
         }
@@ -54,55 +61,63 @@ const game = {
         	$('img').attr("src", "https://media3.giphy.com/media/4eBH2WQPLULLi/source.gif");
         	$('img').css("height", "350px")
         }
-	 }, 600);
+	 }, 900);
   
     },
 
     setHunger () {
-
-    	
 
     	const interval = setInterval(() => { 
 		const $hunger = $('#hunger');
           this.hunger++
        
         $('#hunger').text(`Hunger: ${this.hunger}`);
-        if(this.hunger ===10) {
-    		alert("Tummy rumblers - I died of hunger!");
+
+        if(this.hunger ===10 || this.gameOver===true) {
+    		if (this.hunger ===10){
+    			alert("Tummy rumblers - I died of hunger!");
+    		}
+    		this.gameOver= true;
+    		$('img').css("animation-iteration-count", "0");
     		clearInterval(interval);
     	}
-	 }, 5000);
+	 }, 1000);
 
     	$('#food').on('click', () => {
 			this.hunger --;
 			$('#hunger').text(`Hunger: ${this.hunger}`);
-		});
+		})
     },
 
     setSleepiness () {
     	$('#lights').on('click', () => {
 			this.sleepiness --;
 			$('#sleepiness').text(`Sleepiness: ${this.sleepiness}`);
-	});
+	})
 
     	const interval = setInterval(() => { 
 		const $sleepiness = $('#sleepiness');
           this.sleepiness++
        
         $('#sleepiness').text(`Sleepiness: ${this.sleepiness}`);
-	 	if(this.sleepiness ===10 ) {
+	 	if(this.sleepiness ===10 || this.gameOver===true) {
+
+	 		if (this.sleepiness ===10){
     		alert("Too...much...coding... I died of sleepiness");
+    		}
+    		this.gameOver=true;
+    		$('img').css("animation-iteration-count", "0");
     		clearInterval(interval);
     	}
-	 }, 7500);
+	 }, 750);
     },
 
     setBoredom () {
     	$('#play').on('click', () => {
 			
 			this.boredom --;
-			$('#boredom').text(`boredom: ${this.boredom}`);
-	});
+			$('#boredom').text(`Boredom: ${this.boredom}`);
+	})
 
     	const interval = setInterval(() => { 
 		const $boredom = $('#boredom');
@@ -110,14 +125,20 @@ const game = {
           this.boredom++
        		
         	$('#boredom').text(`boredom: ${this.boredom}`);
-        	if(this.boredom ===10) {
-    		alert("Can you die of boredom? I did!");
+        	if(this.boredom ===10 || this.gameOver ===  true) {
+    		if (this.boredom ===10){
+    			alert("Can you die of boredom? I did!");
+    		}
+    		this.gameOver=true;
+    		$('img').css("animation-iteration-count", "0");
     		clearInterval(interval);
     	}
-	 }, 6430);
+	 }, 2430);
     },
 };
 const joshagotchi = new Gotchi();
+
+
 
 
 
